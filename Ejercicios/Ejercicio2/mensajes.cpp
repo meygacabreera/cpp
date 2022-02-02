70a7e260c22855f7b9fa0ed97f2977c3cefe13d4
Meyga Cabrera <meygacabrera96@gmail.com>
2022-02-01 21:57:14 -0600

    agregando ejercicio 2

---
+++ Ejercicios/Ejercicio2/mensajes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+  using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    string nombre;
+
+    cout <<"ingrese su nombre : ";
+    cin >> nombre;
+
+     cout << endl;
+     
+    cout <<"Hola mi nombre es " << nombre;
+    cout << endl;
+    return 0;
+}
