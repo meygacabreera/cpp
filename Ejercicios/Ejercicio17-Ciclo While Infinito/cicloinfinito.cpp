95cc091bb855708bcca533fce034347d94699ea8
meygacabrera <meygacabrera96@gmail.com>
2022-02-12 14:15:26 -0600

    agregando  ejercicio 17 ciclos while infinito

---
+++ Ejercicios/Ejercicio17-Ciclo While Infinito/cicloinfinito.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  
+  int ciclos = 0;
+  
+    while(ciclo < 100) {
+   cout <<"*";
+   ciclos = ciclos + 1;
+    }
+    
+    return 0;
+}
