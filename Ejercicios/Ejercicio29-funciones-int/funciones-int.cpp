ca06a0ec894e6253083bd055473bff5804f75c4e
meygacabrera <meygacabrera96@gmail.com>
2022-03-07 19:46:47 -0600

    agregando ejercicio29

---
+++ Ejercicios/Ejercicio29-funciones-int/funciones-int.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std; 
+
+int sumar(int a, int b) {
+     return  a + b;
+
+}
+
+int main(int argc, char const *argv[])
+{
+    system("Cls");
+    
+    cout << sumar( 5, 7);
+    cout << endl;
+    cout << sumar( 15, 7);
+    cout << endl;
+    cout << sumar( 10, 7);
+    cout << endl;
+    cout << sumar( 9, 7);
+    cout << endl;
+
+    return 0;
+}
